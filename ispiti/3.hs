d5e5bf59c85f3184a3fea761d2e7c9fe068e7e4e
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2021-05-03 12:50:17 +0200

    dodao 3.hs

---
+++ ispiti/3.hs
@@ -0,0 +1,16 @@
+
+-- radi
+otvori:: [(String, Int)] -> String -> [(String, Int)]
+otvori b br = (br, 0) : b
+
+-- zatvori:: [(String, Int)] -> String -> [(String, Int)]
+
+-- na racun br iz liste b ulati iznos dinara
+uplati:: [(String, Int)] -> String -> Int -> [(String, Int)]
+uplati b br iznos 
+       
+
+
+
+
+
