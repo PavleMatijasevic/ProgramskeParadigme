695465f6a8600132e0d6a61f9bffb965079b9334
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2021-05-09 13:49:20 +0200

    izmenio 3.hs ali  i dalje nije skroz dobar

--- ispiti/3.hs
+++ ispiti/3.hs
@@ -3,13 +3,26 @@
 otvori:: [(String, Int)] -> String -> [(String, Int)]
 otvori b br = (br, 0) : b
 
+porediStringove :: (String, Int) -> String -> String
+porediStringove par zeljeni = (String)par.fst
+
+
+--pronadjiUListi :: [(String, Int)] -> String -> [(String,Int)]
+--pronadjiUListi lista zeljeni = [(x,y) | x <- lista.fst, y <- lista.snd, x /= zeljeni]
+
+
+
+{-
 -- zatvori:: [(String, Int)] -> String -> [(String, Int)]
+zatvori b br 
+zatvori [] br = []
+zatvori (x:xs) br = x : (zatvori(b) xs)
+-}
 
 -- na racun br iz liste b ulati iznos dinara
-uplati:: [(String, Int)] -> String -> Int -> [(String, Int)]
-uplati b br iznos 
+--uplati:: [(String, Int)] -> String -> Int -> [(String, Int)]
+--uplati b br iznos 
        
-
 
 
 
