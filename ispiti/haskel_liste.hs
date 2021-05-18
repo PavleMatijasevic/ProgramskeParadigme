09d5b569656d9dd4a6d37ff6cc8027e90858e7c4
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2021-05-18 19:15:34 +0200

    izmenio haskel_liste.hs

--- ispiti/haskel_liste.hs
+++ ispiti/haskel_liste.hs
@@ -4,7 +4,7 @@ max_liste (x:xs)
             | (max_liste xs) > x = max_liste xs
             | otherwise = x
 
-min_liste :: Ord a -> [a] -> a
+min_liste :: [Float] -> Float
 min_liste [x] = x
 min_liste (x:xs)
     | (min_liste xs) < x = min_liste xs
